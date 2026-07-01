22653b8a257b4515c04eb10adcb78ca701619c48
gmrscw <gmrscw@gmail.com>
2026-07-01 07:13:40 +0530

    Initial commit: To-Do List App

---
+++ script.js
@@ -0,0 +1,30 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value;
+
+    if (task === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        ${task}
+        <button onclick="deleteTask(this)">Delete</button>
+    `;
+
+    li.addEventListener("click", function () {
+        li.style.textDecoration = "line-through";
+    });
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
